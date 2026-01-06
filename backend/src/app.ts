4bdb7deffa1c88dea2b8e18f43a4e4bc9f2f88c5
Jordan Junaidi <jordanrjunaidi@gmail.com>
2026-01-06 14:32:35 -0800

    backend updates

---
+++ backend/src/app.ts
@@ -0,0 +1,33 @@
+import cors from "cors";
+import express from "express";
+
+import { FRONTEND_ORIGIN, PORT } from "./config";
+import errorHandler from "./middleware/errorHandler";
+import log from "./middleware/logger";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: FRONTEND_ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  }),
+);
+
+app.use(express.json());
+
+app.use(log);
+
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.info(`> Listening on port ${PORT}`);
+});
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => {
+//     console.info("Mongoose connected!");
+//     app.listen(PORT, () => {
+//       console.info(`> Listening on port ${PORT}`);
+//     });
+//   })
+//   .catch(console.error);
