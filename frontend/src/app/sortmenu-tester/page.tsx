a971c5b27ce9a671d89ecd640bdabff8205a04fb
Darshika Mishra <darshika@Darshikas-MacBook-Pro.local>
2026-03-12 15:51:21 -0700

    implement pop up feature; still need to add it to show up for list view

---
+++ frontend/src/app/sortmenu-tester/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SortMenuPopup from "@/app/components/SortMenuPopup";
+
+export default function FilterTestPage() {
+  return (
+    <div className="p-10 max-w-[350px]">
+      <SortMenuPopup />
+    </div>
+  );
+}
