10d24e953b96dafd97f7f0c5aa14b9b50fad78f0
Jordan Junaidi <jordanrjunaidi@gmail.com>
2026-03-26 12:40:44 -0700

    Fixed next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,15 +12,6 @@ const nextConfig: NextConfig = {
     ],
   },
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.figma.com",
-        pathname: "/api/mcp/asset/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
